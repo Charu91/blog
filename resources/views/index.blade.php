3d3c44ae871da7f40d5e2b31ccb6431e769a3af7
charu91 <charu@devzila.com>
2015-12-05 15:56:39 +0530

    create  button added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,10 @@
 @extends("layouts.master")
 
 @section('content')
-
+ 
+ <form method='get' action='create'>
+<input type=button onClick="parent.location='create'" value='Create new Blog'>
+</form> 
     @foreach($posts as $post)
         <h3>{{ $post->title }}</h3>
 		<p>{{ $post->text }}</p>
